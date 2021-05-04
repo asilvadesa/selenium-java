a87d4926318ba79227cbcee345f46d0899543ccd
Anderson de Sa <asilvadesa@gmail.com>
2021-05-04 09:39:51 -0400

    Add displayname para teste

--- src/main/java/TesteGoogle.java
+++ src/main/java/TesteGoogle.java
@@ -1,14 +1,26 @@
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.Dimension;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class TesteGoogle {
 
+    private ChromeDriver driver;
+
     @Test
+    @DisplayName("Open browser with selenium for first test")
     public void teste(){
-        WebDriver driver = new ChromeDriver();
+        driver = new ChromeDriver();
         driver.get("http://www.google.com");
+        driver.manage().window().setSize(new Dimension(1200,765));
+        driver.manage().window().maximize();
         Assertions.assertEquals("Google", driver.getTitle());
+    }
+
+    @AfterEach
+    public void close(){
+        driver.quit();
     }
 }
