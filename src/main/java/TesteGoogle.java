cc9ba7f33d17b301183824166c5b180614634eeb
Anderson de Sa <asilvadesa@gmail.com>
2021-05-04 08:46:49 -0400

    Add teste abrir browser com selenium

--- src/main/java/TesteGoogle.java
+++ src/main/java/TesteGoogle.java
@@ -1,2 +1,14 @@
-package PACKAGE_NAME;public class TesteGoogle {
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class TesteGoogle {
+
+    @Test
+    public void teste(){
+        WebDriver driver = new ChromeDriver();
+        driver.get("http://www.google.com");
+        Assertions.assertEquals("Google", driver.getTitle());
+    }
 }
