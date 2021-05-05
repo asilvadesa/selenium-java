bf77d67acdcd3c8d1b759fad44b9e5e55960db46
Anderson de Sá <asilvadesa@gmail.com>
2021-05-05 15:21:06 -0400

    Elementos basicos TextField e TextArea

--- src/main/java/TesteCampoTreinamento.java
+++ src/main/java/TesteCampoTreinamento.java
@@ -1,16 +1,34 @@
-import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class TesteCampoTreinamento {
 
-    WebDriver driver;
+    private WebDriver driver;
 
     @Test
-    @DisplayName("Campo de Treinamento")
-    void teste(){
+    @DisplayName("Deve interagir com TextField")
+    void textFieldTest(){
         driver = new ChromeDriver();
         driver.get("file:///" + System.getProperty("user.dir") + "/src/main/resources/campo_treinamento/componentes.html");
+        driver.findElement(By.id("elementosForm:nome")).sendKeys("Anderson");
+
+        Assertions.assertEquals("Anderson",  driver.findElement(By.id("elementosForm:nome")).getAttribute("value"));
+    }
+
+    @Test
+    @DisplayName("Deve interagir com TextArea")
+    void textAreaTest(){
+        driver = new ChromeDriver();
+        driver.get("file:///" + System.getProperty("user.dir") + "/src/main/resources/campo_treinamento/componentes.html");
+        driver.findElement(By.id("elementosForm:sugestoes")).sendKeys("Teste de Area");
+
+        Assertions.assertEquals("Teste de Area",  driver.findElement(By.id("elementosForm:sugestoes")).getAttribute("value"));
+    }
+
+    @AfterEach
+    void close(){
+        driver.quit();
     }
 }
